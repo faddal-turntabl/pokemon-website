4d5ae61dbf7c02ebb57866aca0ae267547316cad
andoch <andoch.bonin@turntabl.io>
2022-06-30 11:40:16 -0700

    create duplications of everything to get search to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,30 +23,36 @@ export class AppComponent {
   public attack:Number = 0;
   public defence:Number = 0;
 
+  public name2 = "";
+  public hp2:Number = 0;
+  public attack2:Number = 0;
+  public defence2:Number = 0;
+
   ngOnInit() {
     //this.remotePokemonInformation.getPokemonData(this.name).subscribe(data => console.log(data))
     //console.log(this.pokemonData);
   }
 
+  pokemonName = "";
   pokemonHp = 0;
   pokemonAttack = 0;
   pokemonDefence = 0;
 
-  
-  pokemonHp2 = 0;
-  pokemonAttack2 = 0;
-  pokemonDefence2 = 0;
+  pokemonName2 = "";
+  pokemonHp2 = 100;
+  pokemonAttack2 = 100;
+  pokemonDefence2 = 100;
   
 
   displayPokemonStats(searchedName:string, searchNumber:Number): void {
-    this.name = searchedName;
     
-    this.remotePokemonInformation.getPokemonData(this.name).subscribe(data => {
+    this.remotePokemonInformation.getPokemonData(searchedName).subscribe(data => {
       
         if (searchNumber == 1) {
           this.pokemonHp = data.stats[0].base_stat;
           this.pokemonAttack = data.stats[1].base_stat;
           this.pokemonDefence = data.stats[2].base_stat;
+          this.name = searchedName;
           this.hp = this.pokemonHp;
           this.attack = this.pokemonAttack;
           this.defence = this.pokemonDefence;
@@ -55,9 +61,10 @@ export class AppComponent {
           this.pokemonHp2 = data.stats[0].base_stat;
           this.pokemonAttack2 = data.stats[1].base_stat;
           this.pokemonDefence2 = data.stats[2].base_stat;
-          this.hp = this.pokemonHp2;
-          this.attack = this.pokemonAttack2;
-          this.defence = this.pokemonDefence2;
+          this.name2 = searchedName;
+          this.hp2 = this.pokemonHp2;
+          this.attack2 = this.pokemonAttack2;
+          this.defence2 = this.pokemonDefence2;
         }
     });
     //console.log("Health: " + this.pokemonHp);
